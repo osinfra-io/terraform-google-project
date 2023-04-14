6cbbb5ac279ba7006ced8627b63c9eb74d651f65
Brett Curtis <brett@osinfra.io>
2023-04-14 09:00:03 -0400

    Test sensitive outputs

--- test/fixtures/shared/outputs.tf
+++ test/fixtures/shared/outputs.tf
@@ -2,7 +2,8 @@
 # https://www.terraform.io/language/values/outputs
 
 output "cis_2_2_logging_sink_project_id" {
-  value = module.test.cis_2_2_logging_sink_project_id
+  value     = module.test.cis_2_2_logging_sink_project_id
+  sensitive = true
 }
 
 output "project_id" {
