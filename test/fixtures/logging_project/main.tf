231f2ef64b9b41d0b66966be5e45781f5a04f8ee
Brett Curtis <brett@osinfra.io>
2024-02-22 18:29:27 -0500

    Align with Infracost tagging policies

--- test/fixtures/logging_project/main.tf
+++ test/fixtures/logging_project/main.tf
@@ -26,8 +26,9 @@ module "test" {
   folder_id                     = "1069400145815"
 
   labels = {
-    env  = "sb"
-    team = "testing"
+    env        = "sb"
+    team       = "testing"
+    repository = "terraform-google-project"
   }
 
   prefix = "kitchen"
