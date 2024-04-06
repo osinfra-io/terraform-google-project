64f153f2f55d27d1319fd2dcf0534855e3c1fe21
Brett Curtis <brett@osinfra.io>
2024-04-06 09:20:19 -0400

    Deprecation of Kitchen-Terraform Fixes #107

--- fixtures/default/main.tf
+++ fixtures/default/main.tf
@@ -16,14 +16,14 @@ module "test" {
   # This module will be consumed using the source address of the github repo and not the "../../../" used in this test.
   # source = "github.com/osinfra-io/terraform-google-project?ref=v0.0.0"
 
-  source = "../../../global"
+  source = "../../global"
 
   billing_account = var.billing_account
 
   # Since we are defining the CIS 2.2 logging sink project in this test, the google_logging_project_bucket_config
   # resource will not be created.
 
-  cis_2_2_logging_sink_project_id = "plt-lz-audit01-tf92-sb" # This project has been created for tests.
+  cis_2_2_logging_sink_project_id = "plt-lz-audit01-tf92-sb"
   description                     = "test"
   environment                     = "sb"
   folder_id                       = "1069400145815"
@@ -35,7 +35,7 @@ module "test" {
     repository  = "terraform-google-project"
   }
 
-  prefix = "kitchen"
+  prefix = "terraform"
 
   services = [
     "compute.googleapis.com"
