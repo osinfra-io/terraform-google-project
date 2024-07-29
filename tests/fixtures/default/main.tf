099e38b45adfc92ee0160c5602a4a3c22acd375f
Brett Curtis <brett@osinfra.io>
2024-07-29 08:08:38 -0400

    Terraform testing alignment Fixes #121

--- tests/fixtures/default/main.tf
+++ tests/fixtures/default/main.tf
@@ -1,39 +1,30 @@
-# Google Cloud Provider
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs
-
-# The google_billing_budget resource requires this if you are using User ADCs (Application Default Credentials).
-# The following APIs must be enabled on the billing_project:
-# - cloudresourcemanager.googleapis.com
-# - cloudidentity.googleapis.com
-
-provider "google" {
-  billing_project       = "ptl-lz-terraform-tf91-sb"
-  user_project_override = true
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
 }
 
 module "test" {
-
-  # This module will be consumed using the source address of the github repo and not the "../../../" used in this test.
-  # source = "github.com/osinfra-io/terraform-google-project?ref=v0.0.0"
-
   source = "../../../"
 
   # Since we are defining the CIS 2.2 logging sink project in this test, the google_logging_project_bucket_config
   # resource will not be created.
 
-  cis_2_2_logging_sink_project_id = "plt-lz-audit01-tf92-sb"
-  description                     = "test"
-  environment                     = "sb"
-  folder_id                       = "1069400145815"
+  cis_2_2_logging_sink_project_id = "mock"
+  description                     = "mock"
+  environment                     = "mock"
+  folder_id                       = "0000000000000"
 
   labels = {
     cost-center = "x000"
-    env         = "sb"
-    team        = "testing"
-    repository  = "terraform-google-project"
+    env         = "mock"
+    team        = "mock"
+    repository  = "mock"
   }
 
-  prefix = "terraform"
+  prefix = "mock"
 
   services = [
     "compute.googleapis.com"
--- tests/fixtures/logging/main.tf
+++ tests/fixtures/logging/main.tf
@@ -1,34 +1,25 @@
-# Google Cloud Provider
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs
-
-# The google_billing_budget resource requires this if you are using User ADCs (Application Default Credentials).
-# The following APIs must be enabled on the billing_project:
-# - billingbudgets.googleapis.com
-# - cloudresourcemanager.googleapis.com
-
-provider "google" {
-  billing_project       = "ptl-lz-terraform-tf91-sb"
-  user_project_override = true
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
 }
 
 module "test" {
-
-  # This module will be consumed using the source address of the github repo and not the "../../../" used in this test.
-  # source = "git@github.com:osinfra-io/terraform-google-project?ref=v0.0.0"
-
   source = "../../../"
 
   cis_2_2_logging_bucket_locked = false
-  description                   = "test"
-  environment                   = "sb"
-  folder_id                     = "1069400145815"
+  description                   = "mock"
+  environment                   = "mock"
+  folder_id                     = "0000000000000"
 
   labels = {
     cost-center = "x000"
-    env         = "sb"
-    team        = "testing"
-    repository  = "terraform-google-project"
+    env         = "mock"
+    team        = "mock"
+    repository  = "mock"
   }
 
-  prefix = "terraform"
+  prefix = "mock"
 }
