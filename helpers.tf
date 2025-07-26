adbd672e5f54749a95a94e84b0d9da0e580d15c8
Brett Curtis <brett@osinfra.io>
2025-07-26 10:35:44 -0400

    Migrate to OpenTofu (#144)

--- helpers.tf
+++
@@ -1,6 +0,0 @@
-# Terraform Core Child Module Helpers (osinfra.io)
-# https://github.com/osinfra-io/terraform-core-helpers
-
-module "helpers" {
-  source = "github.com/osinfra-io/terraform-core-helpers//child?ref=v0.1.2"
-}
--- tests/fixtures/default/variables.tf
+++
@@ -1,6 +0,0 @@
-# Terraform Input Variables
-# https://www.terraform.io/language/values/variables
-
-variable "cis_2_2_logging_sink_project_id" {
-  type = string
-}
